6e44d5576fd51a35a4dee0c62e79ddca8a286c80
rei0101b <rei0101b@gmail.com>
2020-02-17 03:32:05 +0900

    rubyのコードも載せとく

--- test.rb
+++ test.rb
@@ -9,3 +9,24 @@ end
 Hello.process
 
 puts 'done!'
+
+
+# threads = []
+
+# 10.times do
+#   threads << Thread.new do
+#     count = 0
+
+#     5_000_000.times do
+#       count += 1
+#     end
+
+#     count
+#   end
+# end
+
+# threads.each do |t|
+#   puts "Thread finished with count=#{t.value}"
+# end
+
+# puts "done!"
